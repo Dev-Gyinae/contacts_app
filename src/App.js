5abd12c44f882fe541bfcdab623d2f5938ab1948
Gyinae <dev.gyinae@gmail.com>
2023-05-20 13:49:38 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import {Routes, Route, Navigate} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ContactList from './components/contacts/ContactList/ContactList';
+import AddContact from './components/contacts/AddContact/AddContact';
+import ViewContact from "./components/contacts/ViewContact/ViewContact";
+import EditContact from "./components/contacts/EditContact/EditContact";
 import './App.css';
 
 let App = () => {
@@ -10,8 +13,11 @@ let App = () => {
     <React.Fragment>
       <NavBar />
       <Routes>
-        <Route path={"/"} />
-        <Route path={"/contacts/list"} element={<ContactList/>} />
+        <Route path={"/"} element={<Navigate to={"/contacts/list"} />} />
+        <Route path={"/contacts/list"} element={<ContactList />} />
+        <Route path={"/contacts/add"} element={<AddContact />} />
+        <Route path={"/contacts/view/:contactId"} element={<ViewContact />} />
+        <Route path={"/contacts/edit/:contactId"} element={<EditContact />} />
       </Routes>
     </React.Fragment>
   );
